af895c98cf0ea480eba5668af7576b39c4023a6f
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2024-09-16 10:46:25 +0100

    Update ci.yml to deploy all templates and test response.

---
+++ deployment-tests/salvo-d-test/src/main.rs
@@ -0,0 +1,13 @@
+use salvo::prelude::*;
+
+#[handler]
+async fn hello_world(res: &mut Response) {
+    res.render(Text::Plain("Hello, world!"));
+}
+
+#[shuttle_runtime::main]
+async fn salvo() -> shuttle_salvo::ShuttleSalvo {
+    let router = Router::new().get(hello_world);
+
+    Ok(router.into())
+}
